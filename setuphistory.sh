82201df65ae1395da5a378a5fd2499c428d9cb86
Janne Toivola <jammu81@iki.fi>
2012-10-14 22:17:38 +0300

    maybe hid2hci works?

--- setuphistory.sh
+++ setuphistory.sh
@@ -17,24 +17,43 @@ apt-get install gcc libtool flex bison gdb build-essential
 ## GNU emacs
 apt-get install emacs23-nox emacs23-el python-mode
 
+## xmonad window manager???
+#apt-get install xmonad
+
 
 ## Some Bluetooth support
 apt-get install libbluetooth3 btscanner bluez python-gobject
 
-
 ## Taking Bluetooth HID keyboard into use: hidd deprecated => use hid2hci?
-apt-get install bluez-compat
+#apt-get install bluez-compat
+#
 #hcitool dev
 ## hci0 00:15:83:15:A3:10
-#hidd --search
+#hidd --search # ssh, wired keyboard...
 ## <Press BT button on the NanoX keyboard>
 ## Connecting to device BA:7B:89:11:6E:7C
-echo "HIDD_ENABLED=1" >> /etc/default/bluetooth
-echo 'HIDD_OPTIONS="-i hci0 --connect BA:7B:89:11:6E:7C --server"' >> /etc/default/bluetooth
-#echo hidp | tee -a /etc/modules
+#
+#echo "HIDD_ENABLED=1" >> /etc/default/bluetooth
+#echo 'HIDD_OPTIONS="-i hci0 --connect BA:7B:89:11:6E:7C --server"' >> /etc/default/bluetooth
+##echo hidp | tee -a /etc/modules
 
 ## Some newer BT setup, if hidd gone?
-#sed -i '' -e 's/HID2HCI_ENABLED=0/HID2HCI_ENABLED=1/' /etc/default/bluetooth
+#apt-get install bluetooth bluez-utils blueman # will install X
+## some old thing(?):
+##sed -i '' -e 's/HID2HCI_ENABLED=0/HID2HCI_ENABLED=1/' /etc/default/bluetooth
+#
+#hcitool dev
+## hci0 00:15:83:15:A3:10
+#hcitool scan
+## BA:7B:89:11:6E:7C   Macro Keyboard
+bluez-simple-agent hci0 BA:7B:89:11:6E:7C
+## type in a pin code on rpi (ssh, wired keyboard...)
+## type in the same pin code on NanoX
+bluez-test-device trusted BA:7B:89:11:6E:7C yes
+bluez-test-input connect BA:7B:89:11:6E:7C
+
+#
+## alternative(?):
 #bluetooth-agent 4321
 #rfcomm connect hci0 BA:7B:89:11:6E:7C
 
